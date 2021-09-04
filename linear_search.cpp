e4905cb184202b9901238628bf3473c33ef8affe
Nisham Ghimire <nishamghimire5@gmail.com>
2021-09-04 14:35:49 +0545

    added function

--- linear_search.cpp
+++ linear_search.cpp
@@ -1,39 +1,60 @@
-// #include <iostream>
-// using namespace std;
-// int main() {
-//     int A[10] = {1, 2, 3, 42, 45, 67, 77, 98, 99, 101};
-//     int key,counter = 0;
-//     cout << "Enter searching key : ";
-//     cin >> key;
-//     for (int x : A)
-//     {
-//         if (x == key){
-//             cout << "Found at "<< counter << endl;
-//                 return 0;
-//         }
-//         counter++;
-//     }
-//     cout << "Not found ,sorry." << endl;
-//     return 0;
-//     }
+#include <iostream>
+using namespace std;
+
+int search (int (&a)[10], int key,int *p)
+{
+    int count = 0;
+    for (int x : a)
+    {
+        *p += x;
+        if (x == key)
+            return count;
+
+        count++;
+    }
+    return 0;
+}
+int main() {
+    int A[10] = {1, 2, 3, 42, 45, 67, 77, 98, 99, 101};
+    int key, p, k = 0;
+    cout << "Enter searching key : ";
+    cin >> key;
+    p = search(A, key,&k);
+
+    cout << "Average till the key is : " << (float)k / 10 << endl;
+
+    if(p==0)
+        cout << "Not found ,sorry." << endl;
+    else
+        cout<<"Found at " << p << endl;
+    return 0;
+    }
 
 //NEXT METHOD
 
 
-#include <iostream>
-using namespace std;
-int main() {
-    int A[10] = {1, 2, 3, 42, 45, 67, 77, 98, 99, 101};
-    int key,i = 0;
-    cout << "Enter searching key : ";
-    cin >> key;
-    for (i; i < 10;i++)
-    {
-        if (A[i] == key){
-            cout << "Found at "<< i << endl;
-                exit(0);
-        }
-    }
-    cout << "Not found ,sorry." << endl;
-    return 0;
-    }
+// #include <iostream>
+// using namespace std;
+
+// int search (int A[],int key,int count){
+//     for(int i = 0; i < count; i++){
+//         if(A[i] == key)
+//             return i;
+//     }
+//     return -1;
+// }
+
+// int main() {
+//     int A[10] = {1, 2, 3, 42, 45, 67, 77, 98, 99, 101};
+//     int key,p;
+//     cout << "Enter searching key : ";
+//     cin >> key;
+
+//     p= search(A, key, 10);
+
+//     if(p==-1)
+//         cout << "Not found ,sorry." << endl;
+//     else
+//         cout<<"Found at " << p << endl;
+//     return 0;
+//     }
