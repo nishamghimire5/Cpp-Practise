bb5db38f31b4d5c69c0f9c25f9c7c1af98f32dc3
Nisham Ghimire <nishamghimire5@gmail.com>
2021-09-06 15:07:58 +0545

    privated data

--- class_.cpp
+++ class_.cpp
@@ -3,9 +3,23 @@ using namespace std;
 
 class rectangle
 {
-    public:
+    private:
     int length;
     int breadth;
+
+    public:
+    void setLength(int l){
+        if(l>0)
+            length = l;
+        else
+            length = -l;
+    }
+    void setBreadth(int b){
+        if(b>0)
+            breadth = b;
+        else
+            breadth = -b;
+    }
 
     int area(){
         return length * breadth;
@@ -19,8 +33,8 @@ class rectangle
 int main()
 {
     rectangle *p = new rectangle;
-    p->length = 5;
-    p->breadth = 10;
+    p->setLength(5);
+    p->setBreadth(10);
     cout << "Area of reactangle is : " << p->area() << endl;
     cout << "Perimeter of reactangle is : " << p->perimeter() << endl;
 }
