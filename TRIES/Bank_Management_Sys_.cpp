9cc50cf62e24ab1bf652b89472803f70aad72240
Nisham Ghimire <nishamghimire5@gmail.com>
2021-09-19 12:25:42 +0545

    first print

---
+++ TRIES/Bank_Management_Sys_.cpp
@@ -0,0 +1,18 @@
+/* Bank Management System */
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+
+
+
+
+int main(){
+    cout << "1.  Create an account \n";
+    cout << "2.  Deposit Money \n";
+    cout << "3.  Withdraw Money \n";
+    cout << "4.  Display Account \n";
+    cout << "5.  Exit \n";
+
+    return 0;
+}
